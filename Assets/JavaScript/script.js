9fd7735f2e2b237763f1bd05f3277f681b3d7c78
Michael <mpierson00@gmail.com>
2024-03-13 23:19:39 -0400

    added apikey and button eventlistener

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,6 @@
+const apiKey = '975ba205d260aeafc30576273d2bf34d';
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    const city = document.getElementById('cityInput').value;
+    
+})
