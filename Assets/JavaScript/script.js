a525aa2301e74c9db0bc3eca44222e25a5227d81
Michael <mpierson00@gmail.com>
2024-03-15 00:13:43 -0400

    added forcast data

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -20,9 +20,27 @@ function displayCurrentWeather(data) {
     const currentWeatherHtml = `
     <h2 class="text-lg font-bold">${data.city.name} (${new Date(current.dt * 1000).toLocaleDateString()})</h2>
     <img src="http://openweathermap.org/img/wn/${current.weather[0].icon}.png" alt="Weather icon">
-    <p>Temperature: ${current.main.temp} °C</p>
+    <p>Temperature: ${current.main.temp} °F</p>
     <p>Humidity: ${current.main.humidity}%</p>
     <p>Wind Speed: ${current.wind.speed} m/s</p>
     `;
     document.getElementById('currentWeather').innerHTML = currentWeatherHtml;
+}
+
+function displayForecast(data) {
+    const forecastHtml = '<h2 class="text-lg font-bold">5-Day Forecast:</h2><div class="flex space-x-4">';
+    for (const i = 0; 1 < data.list.length; i += 8) {
+        const forecast = data.list[i];
+        forecastHtml = + `
+       < div class="p-4 bg-white rounded" >
+        <h3>${new Date(forecast.dt * 1000).toLocaleDateString()}</h3>
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png" alt="Weather icon">
+        <p>Temp: ${forecast.main.temp} °F</p>
+        <p>Humidity: ${forecast.main.humidity}%</p>
+        <p>Wind: ${forecast.wind.speed} m/s</p>
+      </div>
+     `;
+    }
+    forecastHtml += '</div>';
+    document.getElementById('forcast').innerHTML = forecastHtml;
 }
